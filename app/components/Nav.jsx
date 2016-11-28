60b6e5a9e004eb3c54ea156e5fe55f2f62723ffe
Harris <halisd1@gmail.com>
2016-11-27 19:11:55 -0800

    Updated Main Page

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -3,7 +3,7 @@ var {Link, IndexLink} = require('react-router');
 
 var Nav = React.createClass({
   onSearch: function(e){
-    e.PreventDefault();
+    e.preventDefault();
     alert('Not Yet Wired Up!');
   },
   render: function(){
