2a33721026d2cd7ecb77307b5a5d82d4e9af2396
Harris <halisd1@gmail.com>
2016-11-27 19:44:15 -0800

    Updated About

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,9 +2,18 @@ var React = require('react');
 
 var About = (props) => {
   return(
-    <div>
-      <h3>About Component</h3>
-      <p>Here is the about page! WOW!</p>
+    <div className = "callout primary large">
+      <h2>About Author,</h2>
+      <p>"Here the author is trying to learn CSS and React JS. Let's wish him luck."</p>
+      <p>Lets revisit this page in November 2017 and see where he is.</p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - This was the JavaScript framework used.
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a> This is the API used to search weather by city.
+        </li>
+      </ul>
     </div>
   )
 };
