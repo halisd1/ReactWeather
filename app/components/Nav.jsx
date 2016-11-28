34a4b47f4a3dd51321f40bb3f25109fc5a11318c
Harris <halisd1@gmail.com>
2016-11-27 18:50:27 -0800

    added NAV foundation

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,15 +1,49 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
-var Nav = (props) =>{
-  return(
-    <div>
-    <h2>Nav Component</h2>
-    <IndexLink to='/'>Get Weather</IndexLink>
-    <Link to='/Examples'>Examples</Link>
-    <Link to='/About'>About</Link>
-    </div>
-  );
-}
+var Nav = React.createClass({
+  onSearch: function(e){
+    e.PreventDefault();
+    alert('Not Yet Wired Up!');
+  },
+  render: function(){
+      return(
+        <div className="top-bar">
+          <div className="top-bar-left">
+            <ul className="menu">
+              <li className="menu-text">React Weather Application</li>
+              <li>
+                <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+              </li>
+              <li>
+                <Link to="/Examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+              </li>
+              <li>
+                  <Link to="/About" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="top-bar-right">
+              <form onSubmit={this.onSearch}>
+                <ul className="menu">
+                  <li>
+                    <input type="search" placeholder="Search Weather"/>
+                  </li>
+                  <li>
+                    <input type="submit" className="button" value="Get Weather"></input>
+                  </li>
+                </ul>
+              </form>
+          </div>
+        </div>
+      );
+  }
+});
 
 module.exports = Nav;
+
+var old = (
+  <div>
+  <h2>Nav Component</h2>
+  </div>
+);
